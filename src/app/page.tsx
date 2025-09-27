b50b48fa9fc35f5c10e9bd2a86c6e7a5b42e3c31
cm-collins <dev-collins@outlook.com>
2025-09-27 19:24:57 +0000

    Initialize Next.js project with TypeScript, Tailwind CSS, and Docker support. Added configuration files, dependencies, and a basic layout with a sample page.

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen' >
+      
+      <button className='bg-blue-500 text-white p-2 rounded-md' >Click me</button>
+    </div>
+  )
+}
+
+export default page
