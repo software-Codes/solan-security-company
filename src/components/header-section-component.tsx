fee25b121ef001093ae4443cc42fa5551df5d2e9
cm-collins <dev-collins@outlook.com>
2025-09-27 21:19:52 +0000

    Update HeaderSectionComponent to improve layout and styling; added rounded class to logo image for better aesthetics.

--- src/components/header-section-component.tsx
+++ src/components/header-section-component.tsx
@@ -29,13 +29,14 @@ export default function HeaderSectionComponent() {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
-          <Link href="/" className="flex items-center space-x-2">
-            <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-gradient-to-r from-orange-500 to-red-500">
+          <Link href="/" className="flex items-center  space-x-2">
+            <div className="flex h-10 w-10 items-center  justify-center rounded-lg bg-gradient-to-r from-orange-500 to-red-500">
               <Image
                 src={solanSecurityLogo}
                 alt="Solan Security Logo"
                 width={150}
                 height={150}
+                className="rounded-full"
               />
             </div>
             <div className="flex flex-col">
